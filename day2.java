4aa6066428f0f2b63aac8feb375846bba5210e85
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-09-09 19:46:42 +0800

    Create day2.java

---
+++ day2.java
@@ -0,0 +1,11 @@
+public class main{
+public static void main(String[] args) {
+  String nama = "Ani Reski Amalia";
+  int umur = 18;
+  String prodi = "Informatika";
+
+  System.out.print("Halo ");
+  System.out.println("semua");
+  System.out.printf(" Nama\t: %s\n Umur\t: %d tahun\n Prodi\t: %s\n", nama, umur, prodi) ;
+}
+}
