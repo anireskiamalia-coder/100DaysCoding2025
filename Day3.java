2d9a6f876de6565d3204a9c7ec79547c4922792b
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-09-10 20:28:22 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,9 @@
+public class main {
+  public static void main(String[] args) {
+    // untuk komentar satu baris
+
+    /* untuk komentar yang lebih dari satu baris */
+
+    /** untuk komentar dokumentasi */
+  }
+}
