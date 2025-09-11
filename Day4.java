1c1fa4aa68ff5352b2642e885ae3873f935d27b2
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-09-11 19:49:25 +0800

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,15 @@
+public class main {
+  public static void main(String[] args) {
+        String nama ="Aniii";
+        String alamat = "Pasangkayu";
+        
+        System.out.println("Nama \t: "+nama);
+        System.out.println("Alamat \t: "+alamat);
+        System.out.println("Nama saya \""+nama+ "\"" + "\nSaya tinggal di \""+alamat+ "\"" );
+
+        /* \t untuk spasi panjang (tab)
+           \n untuk baris baru (newline) 
+           \" untuk kutip ganda
+        */
+  }
+}
