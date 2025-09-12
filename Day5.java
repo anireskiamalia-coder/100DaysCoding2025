6a148f5af58cebbe58e07e57abda50debc2b097b
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-09-12 20:35:01 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,14 @@
+public class main {
+public static void main(String[] args) {
+        int a = 28;
+        byte b = 7;
+        short c = 365;
+        long d = 31536000;
+        
+        System.out.println("jumlah kelas A : "+a+ " orang");
+        System.out.println("Jumlah hari dalam 1 minggu : "+b);
+        System.out.println("Jumlah hari dalam 1 tahun : "+c);
+        System.out.println("Jumlah detik dalam setahun : "+d);
+
+} 
+}
