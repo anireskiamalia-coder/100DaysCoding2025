4d72516c8cb65f56275f7b9e89fa34ab1d2e8e88
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-09-13 15:40:26 +0800

    Delete Day6.java

--- Day6.java
+++
@@ -1,9 +0,0 @@
-public class main {
-  public static void main (String[] args) {
-    float berat = 43.5f;
-    double tinggi = 155,5;
-
-    System.out.println("Berat Badan : + berat);
-    System.out.println("Tinggi Badan : + tinggi);
-  }
-}
