11b4488ec1f2d62cefece83f179a1f6716a845f2
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-09-13 15:37:42 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,9 @@
+public class main {
+  public static void main (String[] args) {
+    float berat = 43.5f;
+    double tinggi = 155,5;
+
+    System.out.println("Berat Badan : + berat);
+    System.out.println("Tinggi Badan : + tinggi);
+  }
+}
