680338683e09119c93adaa86b1567cbbcd2677f6
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-09-14 12:20:41 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,10 @@
+public class main{
+  public static void main(String[] args) {
+        char kelas = 'A';
+        boolean kelassaya = false;
+        
+        System.out.println("Saya dari kelas : "+kelas);
+        System.out.println("Saya dari kelas B : "+kelassaya);
+        
+  }
+}
