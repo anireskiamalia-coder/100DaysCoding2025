5822a84490cc1edd656d7ccffca6941a51d0e149
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-09-15 11:23:12 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,8 @@
+public class main {
+public static void main(String[] args) {
+        String a ="Halo";
+        String b ="java";
+        
+        System.out.println(a+ ", saya sedang belajar "+b+"." );
+    }
+}
