5ddeddd21e8531c8a133fdfa8816cabed0ec9805
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-09-16 21:16:22 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,24 @@
+public class main {
+  public static void main(String[] args) {
+        //deklarasi
+        int tb;
+        float bb;
+        
+        //inisialisasi
+        tb = 153; 
+        bb = 42;    
+        
+        //menampilkan nilai awal
+        System.out.println("Tinggi badan tahun lalu : "+tb);
+        System.out.println("Berat badan tahun lalu : "+bb);
+        
+        //update variable
+        tb = 155;
+        bb = 43.5f;
+        
+        //menampilkan nilai setelah update
+        
+        System.out.println("Tinggi badan tahun lalu : "+tb);
+        System.out.println("Berat badan tahun lalu : "+bb);
+  }
+}
