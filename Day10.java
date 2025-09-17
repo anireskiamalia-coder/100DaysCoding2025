6186f8c0ecc0713556c6bcc4a3446df3ee92c288
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-09-17 20:17:19 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,14 @@
+public class main {
+  public static void main(String[] args) {
+        
+        /*
+        constanta berarti nilai variabel tidak bisa diubah lagi setelah diinisialisasi.
+        di deklarasikan dengan kata kuunci final
+        */
+        
+        final double PHI = 3.14;
+        
+        System.out.println("Nilai Phi : "+PHI);
+
+  }
+}
