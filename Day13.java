28736a851ec7f49860e7ff4c9e40b0c76d228b7a
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-09-20 21:36:37 +0800

    Create Day13.java

---
+++ Day13.java
@@ -0,0 +1,11 @@
+public class main {
+  public static void main(String[] args) {
+        final String NAMALENGKAP = "Ani Reski Amalia";
+        final String NIM = "D0225313";
+        final int TANGGALLAHIR = 15;
+        
+        System.out.println("Nama lengkap : "+NAMALENGKAP);
+        System.out.println("NIM : "+NIM);
+        System.out.println("Tanggal Lahir : "+TANGGALLAHIR);
+  }
+}
