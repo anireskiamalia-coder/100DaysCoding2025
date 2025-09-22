d649ff2f0a7a0202a93302e51747a0912b1048e7
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-09-22 22:23:14 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,13 @@
+public class main {
+  public static void main(String[] args) {
+        int a,b,c,d;
+        a = 5;
+        b = 10;
+        c = a + b;
+        d = b - a;
+        
+        System.out.println(c);
+        System.out.println(d);
+        
+  }
+}
