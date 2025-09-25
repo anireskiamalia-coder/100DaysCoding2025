64cd8e45a557afe2d7d0b39bc7e857a4d363afc9
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-09-25 23:38:58 +0800

    Create Day18.java

---
+++ Day18.java
@@ -0,0 +1,17 @@
+public class main {
+public static void main(String[] args) {
+        byte a = 10;
+        short b = a;
+        int c = b;
+        long d = c;
+        float e = d;
+        double f = e;
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
