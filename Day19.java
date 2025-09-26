7ea48b1f472542b4429e796fe4c2c51f9a98b758
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-09-26 21:35:37 +0800

    Create Day19.java

---
+++ Day19.java
@@ -0,0 +1,18 @@
+public class main {
+  public static void main(String[] args) {
+        double a = 500.55;
+        float b = (float) a;
+        long c = (long) b;
+        int d = (int) c;
+        short e = (short) d;
+        byte f = (byte) e;
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+                
+  }
+}
