b22c7d19ff28e99f81f285a873c62f1b87aaa650
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-09-27 19:19:15 +0800

    Create Day20.java

---
+++ Day20.java
@@ -0,0 +1,30 @@
+public class main {
+  public static void main(String[] args) {
+        byte nilaiByte = 10;
+        short nilaiShort = 100;
+        int nilaiInt = 1000;
+        long nilaiLong = 10000;
+        float nilaiFloat = 3.14f;
+        double nilaiDouble = 123.45;
+        char nilaiChar = 'D';
+        boolean bool = true;
+        
+        String strByte = String.valueOf(nilaiByte);
+        String strShort = String.valueOf(nilaiShort);
+        String strInt = String.valueOf(nilaiInt);
+        String strLong = String.valueOf(nilaiLong);
+        String strFloat = String.valueOf(nilaiFloat);
+        String strDouble = String.valueOf(nilaiDouble);
+        String strChar = String.valueOf(nilaiChar);
+        String strBool = String.valueOf(bool);
+        
+        System.out.println(strByte);
+        System.out.println(strShort);
+        System.out.println(strInt);
+        System.out.println(strLong);
+        System.out.println(strFloat);
+        System.out.println(strDouble);
+        System.out.println(strChar);
+        System.out.println(strBool);
+  }
+}
