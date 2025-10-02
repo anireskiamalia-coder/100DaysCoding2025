02e0b9e777907521c131e4572f6ead76b78df6b9
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-10-02 22:19:06 +0800

    Create Day25.java

---
+++ Day25.java
@@ -0,0 +1,10 @@
+public class main {
+  public static void main(String[] args) {
+        int a = 7;
+        System.out.println("nilai awal : "+a);
+        a++;
+        System.out.println("setelah increment : "+a);
+        a--;
+        System.out.println("setelah decrement : "+a);
+  }
+}
