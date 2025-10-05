614f0aceca0eb335caccdf91c80edd67daadb99a
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-10-05 19:01:41 +0800

    Create Day28.java

---
+++ Day28.java
@@ -0,0 +1,10 @@
+package pkg100daysofcoding;
+public class Day28 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        
+        System.out.println(a == b);
+        System.out.println(a != b);
+    }
+}
