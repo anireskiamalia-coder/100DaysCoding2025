c05339fef36a111fd9a162f9fde206cf5442296a
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-10-10 22:41:43 +0800

    Create Day33.java

---
+++ Day33.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+        boolean akulapar = false;
+
+        System.out.println("Apakah aku lapar? " + akulapar);
+
+        akulapar = !akulapar;
+
+        System.out.println("Apakah aku lapar? " + akulapar);
+    }
+}
