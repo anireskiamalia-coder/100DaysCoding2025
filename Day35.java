4aef351cf74ccdde5ea1557c4bbae26917f1837e
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-10-12 19:50:02 +0800

    Create Day35.java

---
+++ Day35.java
@@ -0,0 +1,13 @@
+package pkg100daysofcoding;
+public class Day35_PercabanganIf {
+    public static void main(String[] args) {
+        int nilaiUjian = 80;
+        int nilaiPraktek = 85;
+        
+        if(nilaiUjian >= 75 && nilaiPraktek >= 75){
+            System.out.println("LULUS");
+        }else{
+            System.out.println("TIDAK LULUS");
+        }
+    }
+}
