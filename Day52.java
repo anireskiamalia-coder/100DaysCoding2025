c3547a371073c9c787ac171f5937f4275443e3ae
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-10-29 21:13:06 +0800

    Create Day52.java

---
+++ Day52.java
@@ -0,0 +1,14 @@
+
+public class day52 {
+    
+    public static void main(String[] args) {
+        int angka = 1; 
+
+        while (angka <= 5) { 
+            System.out.println("Angka ke-" + angka);
+            angka++; 
+        }
+        
+    }
+
+}
