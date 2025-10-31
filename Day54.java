4ea63b8769b863d9d1b338d7df14ccc1d1da5426
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-10-31 16:13:36 +0800

    Create Day54.java

---
+++ Day54.java
@@ -0,0 +1,13 @@
+public class Day54 {
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 20; i++) {
+            
+            if (i % 2 == 0) {
+                continue; 
+            }
+
+            System.out.println("Angka ganjil: " + i);
+        }
+    }
+}
