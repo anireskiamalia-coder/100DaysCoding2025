3814444720d581347fd2a77a8141ebb3a7549ff6
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-11-03 21:12:46 +0800

    Update Day57.java

--- Day57.java
+++ Day57.java
@@ -1,8 +1,8 @@
 
 public class Day57 {
     public static void main(String[] args){
-        for (int i = 0; i < 5; i++) {        // tinggi segitiga
-            for (int j = 0; j < 5; j++) {    // jumlah bintang tiap baris
+        for (int i = 0; i < 5; i++) {        
+            for (int j = 0; j < 5; j++) {  
                 System.out.print("* ");
             }
             System.out.println();
