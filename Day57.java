7678ed08938cc95a6274f2807e19349ea2a5c272
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-11-03 20:09:02 +0800

    Create Day57.java

---
+++ Day57.java
@@ -0,0 +1,12 @@
+
+public class Day57 {
+    public static void main(String[] args){
+        for (int i = 0; i < 5; i++) {        // tinggi segitiga
+            for (int j = 0; j < 5; j++) {    // jumlah bintang tiap baris
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
