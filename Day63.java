fdbd8f07e9f00eb52c337339169b6db45cccbe79
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-11-09 11:55:55 +0800

    Add Day63.java for product calculation
    Implement a Java program to calculate the product of numbers from 1 to N.

---
+++ Day63.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class day63 {
+    public static void main(String args[]){
+      Scanner input = new Scanner(System.in);
+      System.out.print("N: ");
+      int N = input.nextInt();
+      int jumlah = 1;  
+        for(int i = 1; i <= N; i++){
+            System.out.print(i + " ");
+            jumlah *= i;
+        }  
+        
+        System.out.println("\nHasi kali dari 1-"+N+" adalah "+jumlah);
+    }
+}
