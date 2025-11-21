e40cc70dd3bb3cc183b65a4e079bd36e0c038984
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-11-21 20:32:29 +0800

    Add Day75.java for string manipulation example

---
+++ Day75.java
@@ -0,0 +1,10 @@
+package pkg100daysofcoding;
+public class Day74 {
+    public static void main(String[] args) {
+        String nama = "Araa";
+        System.out.println("Jumlah karakter: "+nama.length());
+        System.out.println("Huruf besar: "+nama.toUpperCase());
+        System.out.println("Huruf kecil: "+nama.toLowerCase());
+    }
+    
+}
