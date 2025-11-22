d7a7b52824846750696323f35e29f9bad9dc2ae6
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-11-22 21:16:59 +0800

    Create Day76.java

---
+++ Day76.java
@@ -0,0 +1,15 @@
+package pkg100daysofcoding;
+public class Day76 {
+    public static void main(String[] args) {     
+        String a = "Araa";
+        String b = "araa";
+        String c = "Belajar Java";
+        String d = "";
+
+        System.out.println(a.equals(b));
+        System.out.println(a.equalsIgnoreCase(b));
+        System.out.println(c.contains("Java"));
+        System.out.println(d.isEmpty());
+
+    }
+}
