b0a677b3e067bed18793e367ed4323864be96893
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-11-23 20:37:35 +0800

    Add Day77.java for Java string manipulation examples

---
+++ Day77.java
@@ -0,0 +1,15 @@
+package pkg100daysofcoding;
+import java.util.Scanner;
+public class Day77 {
+    public static void main(String[] args) {
+        String teks = "   Belajar Java itu asik   ";
+
+        System.out.println("Asli: [" + teks + "]");
+        System.out.println("trim(): [" + teks.trim() + "]");
+        System.out.println("substring(3, 10): " + teks.substring(3, 10));
+        System.out.println("replace: " + teks.replace("asik", "susah"));
+    }
+}
+
+        
+    
