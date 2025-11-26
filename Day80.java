94851ac46e134160b68c19539bf010d2889a6d08
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-11-26 20:07:33 +0800

    Add Day80.java with main method and array output

---
+++ Day80.java
@@ -0,0 +1,10 @@
+package pkg100daysofcoding;
+public class Day80 {
+    public static void main(String[] args) {
+        int [] angka = {1,9,7,5};
+        for (int i = 0; i < angka.length; i++) {
+            System.out.print(angka[i]);
+        }
+        System.out.println("");
+    }
+}
