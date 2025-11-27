d24fb42db8323dbe1dc6182b5e5956f441b5410b
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-11-27 20:55:46 +0800

    Create Day81.java

---
+++ Day81.java
@@ -0,0 +1,12 @@
+package pkg100daysofcoding;
+public class Day81 {
+    public static void main(String[] args) {
+        int [] angka = {1,9,7,5};
+        
+        System.out.println("Elemen array indeks 0 = "+angka[0]);
+        System.out.println("Elemen array indeks 1 = "+angka[1]);
+        System.out.println("Elemen array indeks 2 = "+angka[2]);
+        System.out.println("Elemen array indeks 3 = "+angka[3]);
+    }
+    
+}
