c50eac7250170d0f083fe20c9bae376a94fd266d
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-11-29 21:30:52 +0800

    Add Day83.java to calculate sum of an array

---
+++ Day83.java
@@ -0,0 +1,12 @@
+package pkg100daysofcoding;
+public class Day83 {
+    public static void main(String[] args) {
+        int [] angka = {1,9,7,5};
+        int jumlah = 0;
+        for (int i = 0; i < angka.length; i++) {
+            jumlah += angka[i];
+           
+        }
+        System.out.println(jumlah);
+    }
+}
