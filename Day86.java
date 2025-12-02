53f9929a7438a38d7fe987c4071132b1a645fa20
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-12-02 21:53:27 +0800

    Update Day86.java

--- Day86.java
+++ Day86.java
@@ -2,13 +2,13 @@
 package pkg100daysofcoding;
 public class Day86 {
     public static void main(String[] args) {
-        int[] angka = {10, 20, 30, 40, 50};
+        int[] angka = {50,40,30,20,10};
         int min = angka[0];
         for (int i = 0; i < angka.length; i++) {
             if (angka[i] < min) {
                 min = angka[i];
             }
         }
-        System.out.println("Angka tertinggi: "+min);
+        System.out.println("Angka terkecil: "+min);
     }
 }
