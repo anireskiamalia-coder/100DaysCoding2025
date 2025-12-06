48de6a807993d7c98810301a4c0a08429874bd0a
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-12-06 21:31:54 +0800

    Add Day90.java with addition functionality

---
+++ Day90.java
@@ -0,0 +1,12 @@
+package pkg100daysofcoding;
+public class Day90 {
+    public static void main(String[] args) {
+        int hasil = tambah(1,2);
+        System.out.println(hasil);
+
+    }
+    public static int tambah(int a, int b) {
+    return a + b;
+}
+
+}
