fb17206a8fb22086be410e26023e4d0d0be306da
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-12-07 22:20:19 +0800

    Add Day91.java with age printing functionality

---
+++ Day91.java
@@ -0,0 +1,11 @@
+package pkg100daysofcoding;
+public class Day91 {
+    static void Umur(int umur) {
+        System.out.println("Umur saya: " + umur);
+    }
+
+    public static void main(String[] args) {
+        Umur(18);
+    }
+}
+
