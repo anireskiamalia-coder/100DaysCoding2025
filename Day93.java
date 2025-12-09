78d44c34180cac3fc903245adf23eaec4f26cb19
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-12-09 22:19:35 +0800

    Create Day93.java

---
+++ Day93.java
@@ -0,0 +1,14 @@
+package pkg100daysofcoding;
+
+import java.util.Scanner;
+
+public class Day93 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        double d = sc.nextDouble();
+        System.out.println(Math.ceil(d));
+        System.out.println(Math.floor(d));
+        System.out.println(Math.round(d));
+    }
+    
+}
