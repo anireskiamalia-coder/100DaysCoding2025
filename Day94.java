c0e03292ac2bd5e6ea979b3561e55e0e66bb23b9
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-12-10 21:29:50 +0800

    Add Day94.java with a greeting function

---
+++ Day94.java
@@ -0,0 +1,9 @@
+package pkg100daysofcoding;
+public class Day94 {
+    static void cetakNama(String nama){
+        System.out.println("Halo, nama saya "+nama+".");
+    }
+    public static void main(String[] args) {
+        cetakNama("Ani");
+    }
+}
