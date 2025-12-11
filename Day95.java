673cd6e7d37b0020d3196a4818a01c62fa766019
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-12-11 18:24:45 +0800

    Add Day95 class with text and number printing methods

---
+++ Day95.java
@@ -0,0 +1,17 @@
+package pkg100daysofcoding;
+public class Day95 {
+     public static void cetakTeks(String teks) {
+        System.out.println("Teks yang dicetak: " + teks);
+    }
+
+    public static void cetakAngka(int angka) {
+        System.out.println("Angka yang dicetak: " + angka);
+    }
+
+    public static void main(String[] args) {
+     
+        cetakTeks("Hello world");
+        cetakAngka(2025);
+    
+    }
+}
