dc71f32a9df31b1312a7aadf87c96ecef7f06f56
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-12-13 21:40:28 +0800

    Add Day97.java to calculate square area

---
+++ Day97.java
@@ -0,0 +1,9 @@
+package pkg100daysofcoding;
+public class day97 {
+    static int luasPersegi (int sisi){
+        return sisi*sisi;
+    }
+    public static void main(String[] args) {
+        System.out.println(luasPersegi(4));
+    }
+}
