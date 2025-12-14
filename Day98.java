48d4ea6124e7354bd84fbc8fb74ce49a99db761e
anireskiamalia-coder <anireskiamalia@gmail.com>
2025-12-14 23:08:56 +0800

    Add Fibonacci sequence generator program
    Implement Fibonacci sequence generator in Day98.java

---
+++ Day98.java
@@ -0,0 +1,23 @@
+package pkg100daysofcoding;
+import java.util.Scanner;
+public class Day98 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai n: ");
+        int n = sc.nextInt();
+
+        int a = 1, b = 1;
+
+        System.out.print("Deret Fibonacci: ");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
+    
